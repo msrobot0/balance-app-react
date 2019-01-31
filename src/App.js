562c11a5c64d25ab0c215562b5ab7da01d40bc48
Meredith Finkelstein <meredith@printallover.me>
2019-01-31 16:59:20 -0500

    Kinda works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import logo from './meditation.jpeg';
 import './App.css';
+import web3 from './web3';
+import contractinfo from './contractinfo';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      gasUsed:'',
+      txReceipt: '' ,
+      value: null,
+      isConnected:false,
+      account:null,
+      eth:0,
+      totalsupply:0,
+      wha:null,
+      balance:0
+    };
+    web3.eth.net.isListening().then(this.state.isConnected = true)
+  }
+
+  onSubmit = async (event) => {
+		event.preventDefault();
+  };
+
+   componentWillMount() {
+
+   if (!(window.ethereum || window.web3)){
+    web3 = null
+    this.setState({isConnected:false});
+    alert('You have to install MetaMask !');
+    }else{
+        //web3.eth.getAccounts().then(function(results){this.setState({account:results[0]}),this.setState({account:''})});
+    }
+  }
+
+   metamaskLogin = async (event) => {
+		const accounts = await web3.eth.getAccounts();
+
+         this.setState({account:accounts[0]});
+         var err,wei = await web3.eth.getBalance(this.state.account);
+         this.setState({eth:web3.utils.fromWei(wei)});
+         this.setState({balance:contractinfo.methods.balanceOf(accounts[0])});
+         this.setState({totalsupply:contractinfo.methods.totalSupply()});
+		const ethAddress= await contractinfo.options.address;
+		this.setState({wha:ethAddress});
+         const total = await contractinfo.methods.totalSupply().call().then(console.log);
+        alert("hi")
+       //contract.methods.transfer(toAddress, value).send({from: fromAddress})
+//.on('transactionHash', function(hash){
+ // console.log(hash);
+//});
+         //contractinfo.methods.increaseSupply(4);
+         //const contract = await contractinfo.options['address'];
+     //  console.log(contract);
+         //contractinfo.methods.approve(contract,10);
+
+
+    }
+
   render() {
+    const { classes } = this.props;
+    const { spacing } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            get BALANCE
           </p>
+        <h3>Your Balance Stats</h3>
+        <p>{this.state.isConnected?'You are connected to Ropsten test network':'You are not connected'}</p>
+        <Grid container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container  justify="center" spacing={Number(spacing)}>
+              <Grid>
+                <Paper>
+                    <Typography variant="h5" component="h3">
+                            <p>
+                            Total Balance Tokens: {this.state.totalsupply}
+                            </p>
+                    </Typography>
+                </Paper>
+                <Paper>
+                    <Typography variant="h5" component="h3">
+                            <p>
+                            ETH Tokens: {this.state.eth}
+                            </p>
+                    </Typography>
+                </Paper>
+                <Paper>
+                    <Typography variant="h5" component="h3">
+                            <p>
+                            Balance Wallet Address: {this.state.account}
+                            </p>
+                    </Typography>
+                </Paper>
+                <Paper>
+                    <Typography variant="h5" component="h3">
+                            <p>
+                            Eth Wallet Address: {this.state.wha}
+                            </p>
+                    </Typography>
+                </Paper>
+                <Paper>
+                    <Typography variant="h5" component="h3">
+                            <p>
+                            Balance Tokens {this.state.balance}
+                            </p>
+                    </Typography>
+                </Paper>
+              </Grid>
+          </Grid>
+        </Grid>
+        </Grid>
+        <Button variant="outlined"  onClick={() => this.metamaskLogin()}>
+          breathe
+        </Button>
+        <Button variant="outlined">
+         Social Media
+        </Button>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/consciouscomputation"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Computer Tooling Consciousness
           </a>
         </header>
       </div>
---
+++ src/contractinfo.js
@@ -0,0 +1,419 @@
+import web3 from './web3';
+const address='0x93cf363076fa8723ba838f9a940cdde66a1296a9';
+const abi = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x06fdde03"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "DECIMALS",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x2e0f2625"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "INITIAL_SUPPLY",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x2ff2e9dc"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x313ce567"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "name": "addedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "increaseAllowance",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0x39509351"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0x715018a6"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x8da5cb5b"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "isOwner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x8f32d59b"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x95d89b41"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "name": "subtractedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "decreaseAllowance",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0xa457c2d7"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0xf2fde38b"
+    },
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor",
+      "signature": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event",
+      "signature": "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event",
+      "signature": "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event",
+      "signature": "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x18160ddd"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_amt",
+          "type": "uint256"
+        }
+      ],
+      "name": "increaseSupply",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0xb921e163"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_amt",
+          "type": "uint256"
+        }
+      ],
+      "name": "decreaseSupply",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0x98e52f9a"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "tokenOwner",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "name": "balance",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0x70a08231"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "name": "tokens",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0x095ea7b3"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "tokenOwner",
+          "type": "address"
+        },
+        {
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "name": "remaining",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function",
+      "signature": "0xdd62ed3e"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "name": "tokens",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0xa9059cbb"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "name": "tokens",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function",
+      "signature": "0x23b872dd"
+    }
+  ];
+export default new web3.eth.Contract(abi,address);
